d5245504d425da767c86a4b105cee10157143693
Kartik <kartik.ipatil10@gmail.com>
2022-09-03 18:18:16 +0530

    Feat: Handle click - move the bird up and down

---
+++ index.js
@@ -0,0 +1,29 @@
+const starBtn = document.getElementById("start-btn");
+const buttonContainer = document.querySelector(".button-container");
+const scoreContainer = document.querySelector(".score-container");
+const score = document.getElementById("score");
+const bird = document.getElementById("bird");
+const gameContainer = document.getElementById("game-container");
+
+starBtn.addEventListener("click", () => {
+    let count = 0;
+    buttonContainer.style.display = "none";
+    scoreContainer.style.display = "none";
+    
+    
+    setInterval(() => {
+        console.log("bird.style.marginTop", bird.style.marginTop)
+        bird.style.marginTop = Number(bird.style.marginTop.slice(0, -2)) + 50 + "px";
+        score.innerText = count;
+        count++;
+    }, 1000)
+})
+
+gameContainer.addEventListener("click", () => {
+    bird.style.marginTop = bird.style.marginTop.slice(0, -2) - 50 + "px";
+
+    if (bird.style.marginTop.slice(0, -2) == "-450") {
+        alert("Score", score);
+        return;
+    }
+})
